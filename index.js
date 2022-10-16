53aeab52d6a83e172fc1b792d79c71cc6d819589
Daniel W <whiteside.danielj@gmail.com>
2022-10-15 20:34:01 -0500

    Update index.js
    add comments and object for candidate-committee-linkage

--- index.js
+++ index.js
@@ -20,22 +20,22 @@ class QueryObject {
 
 
 //create a new object using QueryObject class this new 
-//object is specifically for the 'all candidates' datasheet
 
+//object is specifically for the 'all candidates' datasheet
 const allCandidates = new QueryObject(
     'all_candidates', 
     ['CAND_ID', 'CAND_NAME', 'CAND_ICI', 'PTY_CD', 'CAND_PTY_AFFILIATION', 'TTL_RECEIPTS', 'TRANS_FROM_AUTH', 'TTL_DISB', 'TRANS_TO_AUTH', 'COH_BOP', 'COH_COP', 'CAND_CONTRIB', 'CAND_LOANS', 'OTHER_LOANS', 'CAND_LOAN_REPAY', 'OTHER_LOAN_REPAY', 'DEBTS_OWED_BY', 'TTL_INDIV_CONTRIB', 'CAND_OFFICE_ST', 'CAND_OFFICE_DISTRICT', 'SPEC_ELECTION', 'PRIM_ELECTION', 'RUN_ELECTION', 'GEN_ELECTION', 'GEN_ELECTION_PERCENT' ,'OTHER_POL_CMTE_CONTRIB', 'POL_PTY_CONTRIB', 'CVG_END_DT', 'INDIV_REFUNDS','CMTE_REFUNDS'], 
     ['string','string','string','string','string','float','float','float','float','float','float','float','float','float','float','float','float','float','string','string','string','string','string','string','float','float','float','date','float','float'],
     '../raw_data/all_candidates/weball22.txt'
     )
-
+//object is specifically for the 'candidate-committee-linkage' datasheet
 const candidateCommitteeLink = new QueryObject(
     'candidate_committee_link', 
     ['CAND_ID',	'CAND_ELECTION_YR','FEC_ELECTION_YR','CMTE_ID','CMTE_TP','CMTE_DSGN','LINKAGE_ID'], 
     ['string','string','string','string','string','string','string'],
     '../raw_data/candidate_committee_link/ccl.txt'
     )
-
+//object is specifically for the 'individual contributions' datasheet
 const individualContributions = new QueryObject(
     'individual_contributions', 
     ['CMTE_ID','AMNDT_IND','RPT_TP','TRANSACTION_PGI','IMAGE_NUM','TRANSACTION_TP','ENTITY_TP','NAME','CITY','STATE','ZIP_CODE','EMPLOYER','OCCUPATION','TRANSACTION_DT','TRANSACTION_AMT','OTHER_ID','TRAN_ID','FILE_NUM','MEMO_CD','MEMO_TEXT','SUB_ID'], 
