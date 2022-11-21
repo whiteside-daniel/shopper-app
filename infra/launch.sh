faeb894011ee13fcfcdc7afe3c7ce659d711f4dd
Brian Wijayawardana <bwijayaw@Brian-Wijayawardana-BrainGu.local>
2022-11-21 02:15:03 -0600

    Updated public ip of NFS server

--- infra/launch.sh
+++ infra/launch.sh
@@ -6,5 +6,5 @@ sudo systemctl restart nfs-utils
 sudo systemctl status nfs-utils
 
 sudo mkdir -p /mnt/nfs_vagrant
-sudo mount -v -t nfs -o port=4747 192.168.1.215:/nfs_shared /mnt/nfs_vagrant
+sudo mount -v -t nfs -o port=4747 64.109.199.107:/nfs_shared /mnt/nfs_vagrant
 nfsstat -m
